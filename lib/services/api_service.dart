e488cd740f242f88376247c79ef1b62fb17faca4
Yashwanth Sai Sollu <www.sai1929@gmail.com>
2024-07-31 17:17:33 +0530

    Defined api service to retreive data from cosmocloud api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,1 +1,47 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/employee.dart';
 
+class ApiService {
+  final String baseUrl =
+      'https://free-ap-south-1.cosmocloud.io/development/api/employee_manager';
+
+  Future<List<Employee>> getEmployees() async {
+    final response = await http.get(Uri.parse('$baseUrl?limit=100&offset=0'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body)['data'];
+      return data.map((json) => Employee.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load employees');
+    }
+  }
+
+  Future<Employee> getEmployeeById(String id) async {
+    final response = await http.get(Uri.parse('$baseUrl/$id'));
+    if (response.statusCode == 200) {
+      return Employee.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load employee');
+    }
+  }
+
+  Future<void> createEmployee(Employee employee) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(employee.toJson()),
+    );
+    if (response.statusCode == 201) {
+    } else {
+      throw Exception('Failed to create employee');
+    }
+  }
+
+  Future<void> deleteEmployee(String id) async {
+    final response = await http.delete(Uri.parse('$baseUrl/$id'));
+    if (response.statusCode == 204) {
+    } else {
+      throw Exception('Failed to delete employee');
+    }
+  }
+}
